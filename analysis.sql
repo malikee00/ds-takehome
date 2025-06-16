bb99f8af19f81da5e8484867e4085355a3a363cd
malikee00 <strongranger44@gmail.com>
2025-06-16 20:31:42 +0700

    Add anomaly detection query and findings

--- analysis.sql
+++ analysis.sql
@@ -1,7 +1,7 @@
 CREATE TABLE transaction (
     order_id VARCHAR(64),
     customer_id VARCHAR(64),
-    order_date_raw VARCHAR(20),  -- ubah sementara jadi string
+    order_date_raw VARCHAR(20),  -- into string
     payment_value DECIMAL(10,2),
     decoy_flag VARCHAR(10),
     decoy_noise VARCHAR(10)
@@ -35,3 +35,67 @@ SELECT
 FROM transaction
 GROUP BY customer_id
 ORDER BY CAST(customer_id AS UNSIGNED);
+
+-- QUERY: Anomaly Detection
+-- sneak peek for knowing top 10 abs_diff
+SELECT 
+  order_id,
+  customer_id,
+  order_date_raw,
+  payment_value,
+  decoy_flag,
+  decoy_noise,
+  ABS(payment_value - decoy_noise) AS abs_diff
+FROM transaction
+ORDER BY abs_diff DESC
+LIMIT 10;
+
+-- MEASURING MOST ANOMALIEST DECOY_FLAG
+-- Knowing the treshold
+
+SELECT COUNT(*) AS total_rows FROM abs_diff_values;
+
+-- Q1
+SELECT abs_diff FROM abs_diff_values
+LIMIT 1 OFFSET 2499;  -- 25% dari 10.000
+
+-- Q3
+SELECT abs_diff FROM abs_diff_values
+LIMIT 1 OFFSET 7499;  -- 75% dari 10.000
+
+-- 11.069999999999993 (Q1)
+-- 41.16999999999996 (Q2)
+-- 30 (IQR = Q3 - Q1)
+
+SELECT 41.17 + 1.5 * (41.17 - 11.07) AS threshold;
+-- 86.320 (Treshold)
+
+-- 1. Merging total transaction on CTE
+WITH 
+total_trans AS (
+    SELECT 
+        decoy_flag,
+        COUNT(*) AS total_trans
+    FROM transaction
+    GROUP BY decoy_flag
+),
+
+anomaly_transactions AS (
+    SELECT 
+        decoy_flag,
+        COUNT(*) AS anomaly_count
+    FROM transaction
+    WHERE ABS(payment_value - decoy_noise) > 86.32
+    GROUP BY decoy_flag
+)
+
+-- Adding them and measure the anomaly percentage
+SELECT 
+    t.decoy_flag,
+    t.total_trans,
+    COALESCE(a.anomaly_count, 0) AS anomaly_count,
+    ROUND(COALESCE(a.anomaly_count, 0) / t.total_trans * 100, 2) AS anomaly_pct
+FROM total_trans t
+LEFT JOIN anomaly_transactions a
+    ON t.decoy_flag = a.decoy_flag
+ORDER BY anomaly_pct DESC;
