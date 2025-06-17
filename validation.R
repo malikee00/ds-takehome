5de94c56a1da2aec70a030b90ec2a0beb8640cf8
malikee00 <strongranger44@gmail.com>
2025-06-17 15:15:44 +0700

    Hosmer-Lemeshow test

--- validation.R
+++ validation.R
@@ -13,3 +13,16 @@ model <- glm(default ~ credit_score + loan_amount + monthly_income + age,
              family = binomial, data = df)
 
 model
+
+# Hosmer-Lemeshow test
+hltest(model, g = 10) # g = 10 grup probabilitas
+
+df$predicted <- predict(model, type = "response")
+df$bin <- ntile(df$predicted, 10)
+
+calib <- df %>%
+  group_by(bin) %>%
+  summarise(
+    avg_pred = mean(predicted),
+    actual = mean(default)
+  )
